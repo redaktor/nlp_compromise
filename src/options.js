a14f5badfc75105b3dc08f65865254008aec588a
redaktor <api@redaktorcms.com>
2015-06-14 18:09:36 +0200

    a proposal for handling options persistent
    stub, WIP, not used yet.

---
+++ src/options.js
@@ -0,0 +1,22 @@
+// TODO
+// caching by sentence, so unchanged-sentences aren't re-parsed on keystroke -> set: function (key, val) { nlpOptions.nlp.sentences[key] = val; } (hashing ?)
+
+exports.nlpOptions = {
+	cache: {db:false},
+	pos: {dont_combine:true},
+	ngram: {min_count:1, max_size:5},
+	normalize: {percentage:50}
+};
+module.exports = function (db) {
+		if (typeof module !== 'undefined' && module.exports) {
+			exports.nlpOptions = require.nlpOptions;
+		} else if (typeof window != 'undefined') {
+			exports.nlpOptions = window.nlpOptions;
+		}
+		nlpOptions.nlp = {};
+
+    return {
+        get: function (key) { return exports.nlpOptions.nlp[key]; },
+        set: function (key, val) { return exports.nlpOptions.nlp[key] = val; }
+    }
+};
