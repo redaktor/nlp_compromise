1c4f0fbb4db5a1a0051d9ec661175be0a789096f
redaktor <api@redaktorcms.com>
2015-06-17 00:17:47 +0200

    formatting

--- src/methods/tokenization/tokenize.js
+++ src/methods/tokenization/tokenize.js
@@ -1,15 +1,16 @@
-//split a string into 'words' - as intended to be most helpful for this library.
+// split a string into 'words' - as intended to be most helpful for this library.
 
 if (typeof lang != 'string') lang = 'en';
 var multiples = require('../../data/'+lang+'/multiples');
 var sentence_parser = require('./sentence');
 
-//these expressions ought to be one token, not two, because they are a distinct POS together
+// these expressions ought to be one token, not two, because they are a distinct POS together
 var multi_words = Object.keys(multiples).map(function(m) {
 	return m.split(' ');
 });
 
 function normalise (str) {
+	// TODO - does it handle all european languages?
 	if (!str) { return "" }
 	str = str.toLowerCase();
 	str = str.replace(/[,\.!:;\?\(\)]/, '');
@@ -30,17 +31,17 @@ function sentence_type(sentence) {
 	return 'declarative';
 }
 
-//some multi-word tokens should be combined here
+// some multi-word tokens should be combined here
 function combine_multiples(arr) {
 	var better = [];
 	var normalised = arr.map(function(a){
 		return normalise(a)
-	}); //cached results
+	}); // cached results
 	
 	arr.forEach(function(w, i) {
 		for (var o = 0; o < multi_words.length; o++) {
 			if (arr[i + 1] && normalised[i] === multi_words[o][0] && normalised[i+1] === multi_words[o][1]) { 
-				//we have a match
+				// we have a match
 				arr[i] = w + ' ' + arr[i + 1];
 				arr[i + 1] = null;
 				break;
