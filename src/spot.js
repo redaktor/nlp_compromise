8be2601fa905b4fbc2818c64e266abc678eddcae
redaktor <api@redaktorcms.com>
2015-06-17 21:25:01 +0200

    format

--- src/spot.js
+++ src/spot.js
@@ -1,18 +1,18 @@
 // just a wrapper for text -> entities
 // most of this logic is in ./parents/noun
 var pos = require("./pos");
-//::
+
 exports.main = function(text, options) {
 	options = options || {};
-	//collect 'entities' from all nouns
+	// collect 'entities' from all nouns
 	var sentences = pos(text, options).sentences;
 	var arr = sentences.reduce(function(arr, s) {
 		return arr.concat(s.entities(options));
 	}, []);
-	//for people, remove instances of 'george', and 'bush' after 'george bush'.
+	// for people, remove instances of 'george', and 'bush' after 'george bush'.
 	var ignore = {};
 	arr = arr.filter(function(o){
-		//add tokens to blacklist
+		// add tokens to blacklist
 		if(o.analysis.is_person()){
 			o.normalised.split(' ').forEach(function(s){
 				ignore[s] = true;
