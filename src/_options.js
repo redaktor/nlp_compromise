16a08dc0b5ce718111a60098ddc340ac8e87eff5
redaktor <api@redaktorcms.com>
2015-06-22 16:01:33 +0200

    added comment

--- src/_options.js
+++ src/_options.js
@@ -4,6 +4,10 @@
  * WIP <br>
  * @module src/_options
  */
+
+// note: if we want the alternative of setting the subkeys flat,
+// like {combine:false, min_count:2}, they must be unique ...
+ 
 // TODO - WIP
 module.exports = {
 	cache: {
