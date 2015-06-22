71ede3b84565b798c3afb2a431ef1d96830ac099
redaktor <api@redaktorcms.com>
2015-06-22 15:55:04 +0200

    removed forgotten comments

--- src/parents/verb/conjugate/to_doer.js
+++ src/parents/verb/conjugate/to_doer.js
@@ -9,7 +9,6 @@ var verbs_conjugate = require('../../../data/'+lang+'/verbs/conjugate');
 
 exports.main = function(str) {	
 	str = str || '';
-	/// console.log( str );
 	
 	var transforms = [{
 		reg: /e$/i,
@@ -26,23 +25,19 @@ exports.main = function(str) {
 	}];
 
 	if (verbs_conjugate.noDoers.hasOwnProperty(str)) {
-		/// console.log( '0', str );
 		return null;
 	}
 	if (verbs_conjugate.irregularDoers.hasOwnProperty(str)) {
-		/// console.log( '1', verbs_conjugate.irregularDoers[str] );
 		return verbs_conjugate.irregularDoers[str];
 	}
 	for (var i = 0; i < transforms.length; i++) {
 		if (str.match(transforms[i].reg)) {
 			var replaced = str.replace(transforms[i].reg, transforms[i].repl);
-			/// console.log( '2', str, replaced, str.replace(transforms[i].reg, transforms[i].repl) );
 			if (replaced) {
 				return str.replace(transforms[i].reg, transforms[i].repl);
 			}
 		}
 	}
-	/// console.log('3', str + 'er');
 	return str + 'er';
 }
 module.exports = exports.main;
