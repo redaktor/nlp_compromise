e718464eb4d4c017c8c79b6c8b345ce8699e548d
redaktor <api@redaktorcms.com>
2015-07-20 17:10:07 +0200

    deleted inspectFn
    was only used internally by _build.
    Is now much smaller and part of _build.

--- src/data/_.js
+++ src/data/_.js
@@ -9,63 +9,6 @@
  * @module data/_
  */
 var dict = require('./dictionary');
-function inspectFn(depth, ctx) {
-	// 
-	var m = ['{\n\t'];
-	var x = 0;
-	var l = Object.keys(this).length-1;
-	var level = 0; // TODO
-	
-	function format(t, a, k) {
-		if (!a) { a = []; }
-		if (k) { a.push('\t', k, ': '); }
-		if (typeof t === 'object' && !(t instanceof Array) && !(t instanceof RegExp)) {
-			a = a.concat(formatO(t));
-		} else if (typeof t === 'string') {
-			a.push("'", t.replace("'", '\\\''), "'");
-		} else {
-			a.push((typeof t === 'function' || t instanceof RegExp) ? (t.toString()) : (t));
-		}
-		return a;
-	}
-	
-	function formatO(o) {
-		var om = ['{\n\t\t'];
-		var ox = 0;
-		var ol = Object.keys(o).length;
-		for (var k in o) {
-			ox++;
-			var _t = o[k];
-			if (_t instanceof Array) {
-				var fA = _t.map(function(t){ return format(t).join(''); });
-				om.push('\t', k, ': [', fA, ']');
-			} else {
-				om = format(_t, om, k);
-			}
-			om.push((ox < ol) ? ',\n\t\t' : '\n\t\t');
-		}
-		om.push('}');
-		return om;
-	}
-	for (var key in this) {
-		if (key != 'inspect') {
-			x++;
-			var _t = this[key];
-			if (typeof _t === 'object' && !(_t instanceof Array) && !(_t instanceof RegExp)) {
-				m.push(key, ': ');
-				m = m.concat(formatO(_t));
-			} else if (typeof _t === 'string') {
-				m.push(key, ': ', "'", _t, "'");
-			} else {
-				m.push(key, ': ', (typeof _t === 'function' || _t instanceof RegExp) ? (_t.toString()) : (_t));
-			}
-			m.push((x<l) ? ',\n\t' : '\n');
-		}
-	}
-	m.push('}');
-	return m.join('');
-}
-
 var lang = 'en';
 var results = {main:[[]], zip:[[]]};
 
@@ -93,7 +36,10 @@ function newRes(isZip) {
 	if (r.length-1 > 0) results[(isZip) ? 'zip' : 'main'].push([]);
 	return [];
 }
-function val(o) {
+function val(o, fb) {
+	if (fb) {
+		return (o.hasOwnProperty(lang)) ? o[lang] : fb;
+	}
 	return (typeof o === 'string') ? o : o[lang]; 
 }
 function did(s, isZip) {
@@ -148,7 +94,6 @@ function meta(o, i) {
 	return false;
 }
 module.exports = {
-	inspectFn: inspectFn,
 	allPossible: allPossible,
 	setLang: setLang,
 	getRes: getRes,
