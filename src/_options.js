d4c562f02ee9b54fb26e8aaf187b848d998cc6ba
redaktor <api@redaktorcms.com>
2015-07-20 17:05:26 +0200

    default options
    - new options for dates
      - 'now' to parse (more) relative dates
      - 'locale' overrides 'lang', can be e.g. en-ca
      - 'localized' determines the formatting of the localized date

--- src/_options.js
+++ src/_options.js
@@ -1,5 +1,5 @@
 /**
- * default OPTIONS <br>
+ * predefined default OPTIONS <br>
  * for mixin in modules <br>
  * WIP <br>
  * @module src/_options
@@ -8,7 +8,7 @@
 // note: if we want the alternative of setting the subkeys flat,
 // like {combine:false, min_count:2}, they must be unique ...
  
-// TODO - WIP
+// TODO - jsDOC
 module.exports = {
 	cache: {
 		size: 256, 
@@ -16,6 +16,19 @@ module.exports = {
 	},
 	pos: {
 		combine: true
+	},
+	numbers: {
+		// TODO	
+	},
+	dates: {
+		strict: false,
+		// provide a valid JS Date or JSON Date to parse relative dates too
+		now: null,
+		// optional date locale can be set here,
+		// see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DateTimeFormat#Parameters
+		locale: false,
+		// see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DateTimeFormat#Using_options
+		localized: { timeZone: 'UTC', weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 	},
 	ngram: {
 		min_count: 1, 
