3b21f69fdc5306b00bb72b2abbc95883bc5c8b1e
redaktor <api@redaktorcms.com>
2015-07-20 19:13:52 +0200

    make value methods interact:
    note: the returned arrays also have a .text() method [see 2]
    
    1) discover all numerals
    2) e.g. date can use a numeric version of the text [Fourth of July is
    4th of July]
    
    due to arrays (by convention) 'is_date' is now 'has_date' and there is
    also a 'has_number' method - see to_date and to_number commit messages
    of this squashed commit...

--- src/parents/value/index.js
+++ src/parents/value/index.js
@@ -4,38 +4,46 @@
  */
 if (typeof lang != 'string') lang = 'en';
 var schema = require('../../data/'+lang+'/schema');
-var dates = require('../../data/'+lang+'/lexicon/dates');
+var _ = require('../../_');
 var cache = require('../../cache');
+var to_date = require('./to_date');
 var to_number = require('./to_number');
-var date_extractor = require('./date_extractor');
-	
-module.exports = function(str, sentence, word_i) {
-	var cached = null;
-  var the = this;
-  the.word = str || '';
-	
-  the.is_date = function() {
-		return (the.word.match(new RegExp(dates.monthS, 'i')) || the.word.match(new RegExp(dates.dayS, 'i')) || the.word.match(/1?[0-9]:[0-9]{2}/));
-	}
-	
-  the.date = function(options) {
-    return date_extractor(the.word, options);
-  }
+var rNr = /(\d+)(?=\D|$)/g;
 
-  the.number = function() {
-    if (the.is_date()) { return null; }
-    return to_number(the.word);
-  }
-
-	cached = cache.get(the.word, 'valueWhich');
-  the.which = (cached) ? cached : cache.set(the.word, (function() {
-    if (the.date()) { return schema['DA']; }
-    if (the.number()){ return schema['NU']; }
-    return schema['CD'];
-  })(), 'valueWhich');
-
-  return the;
+function which() {
+	if (this._date) { return schema['DA']; }
+	if (this._number){ return schema['NU']; }
+	return schema['CD'];
 }
+function numeralInDates(o) {
+	var m = o.text.match(rNr), i;
+	if (_.hasL(m)) {
+		for (i=0; i<m.length; i++) {
+			var nr = parseInt(m[i],10);
+			var index = isNaN(nr) ? -1 : this.numerals.indexOf(nr);
+			if (index > -1) { this.numerals.splice(index, 1); }
+		}
+	}
+}
+function value(str, sentence, word_i) {
+  this.word = str || '';
+	this.numerals = to_number.prototype.hasNumeral(this.word);
+	this.numbers = [];
+	this.dates = [];
+	this.values = [];
+	this.date();
+	this.dates.forEach(numeralInDates.bind(this));
+	
+	this.number();
+	cached = cache.get(this.word, 'valueWhich');
+  this.which = (cached) ? cached : cache.set(this.word, (which.bind(this))(), 'valueWhich');
+  return this;
+}
+value.prototype.has_date = function(){ return _.hasL(this.dates); };
+value.prototype.has_number = function(){ return _.hasL(this.numbers); };
+value.prototype.date = to_date;
+value.prototype.number = to_number;
 
+module.exports = value;
 // console.log(new Value("fifty five").number())
 // console.log(new Value("june 5th 1998").date())
